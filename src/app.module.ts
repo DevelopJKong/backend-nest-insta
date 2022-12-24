d1c9c53c19781c118f291c88a9eda63d6210289a
cafe small house <ybaram40@gmail.com>
2022-12-24 17:03:05 +0900

    #3.11 Dotenv 완료

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MoviesModule } from './movies/movies.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [MoviesModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService],
 })
