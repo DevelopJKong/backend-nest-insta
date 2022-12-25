affdfa16d33183efb9a61939d8a0ce29f6ae034a
cafe small house <ybaram40@gmail.com>
2022-12-25 11:58:08 +0900

    #4.3 seeProfile 완료-2

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,25 +1,44 @@
-import { Module } from '@nestjs/common';
-import { PrismaModule } from './prisma/prisma.module';
-import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
-import { UsersModule } from './users/users.module';
+import { Module } from "@nestjs/common";
+import { PrismaModule } from "./prisma/prisma.module";
+import { GraphQLModule } from "@nestjs/graphql";
+import { ApolloDriver, ApolloDriverConfig } from "@nestjs/apollo";
+import { UsersModule } from "./users/users.module";
+import { LoggerModule } from "./libs/logger/logger.module";
+import { ConfigModule } from "@nestjs/config";
+import * as Joi from "joi";
 
 @Module({
   imports: [
+    // ! 피리즈마 설정 모듈
     PrismaModule,
+    // ! ENV 설정 모듈
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.test",
+      ignoreEnvFile: process.env.NODE_ENV === "prod",
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().valid("dev", "prod").required()
+      })
+    }),
+    // ! GraphQL 설정 모듈
     GraphQLModule.forRoot<ApolloDriverConfig>({
       installSubscriptionHandlers: true,
       driver: ApolloDriver,
       autoSchemaFile: true,
       context: ({ req, connection }) => {
-        const TOKEN_KEY = 'x-jwt';
+        const TOKEN_KEY = "x-jwt";
         return {
-          token: req ? req.headers[TOKEN_KEY] : connection.context[TOKEN_KEY],
+          token: req ? req.headers[TOKEN_KEY] : connection.context[TOKEN_KEY]
         };
-      },
+      }
     }),
-    UsersModule,
+    // ! Logger 설정 모듈
+    LoggerModule.forRoot({
+      nodeEnv: process.env.NODE_ENV
+    }),
+    UsersModule, // ! 유저 모듈
   ],
-  providers: [],
+  providers: []
 })
-export class AppModule {}
+export class AppModule {
+}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,26 +2,34 @@ import { Injectable } from "@nestjs/common";
 import { PrismaService } from "../prisma/prisma.service";
 import { GetUserInput, GetUserOutput } from "./dtos/get-user.dto";
 import { CreateUserInput, CreateUserOutput } from "./dtos/create-user.dto";
+import * as bcrypt from "bcrypt";
+import { LoggerService } from "../libs/logger/logger.service";
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {
+  constructor(private readonly prisma: PrismaService, private readonly log: LoggerService) {
   }
 
   async getUser({ id }: GetUserInput): Promise<GetUserOutput> {
     try {
+      // ! 데이터베이스에서 유저 찾기
       const user = await this.prisma.user.findUnique({
         where: {
           id
         }
       });
-      console.log(user);
+
+      // * 유저가 있을 경우
+      this.log.logger().info(`${this.log.loggerInfo("유저 찾기")}`);
       return {
         ok: true,
         user
       };
 
     } catch (error) {
+      //! extraError
+      const { message, name, stack } = error;
+      this.log.logger().error(`${this.log.loggerInfo("extraError", message, name, stack)}`);
       return {
         ok: false,
         error: "User not found"
@@ -31,6 +39,7 @@ export class UsersService {
 
   async createUser({ firstName, lastName, email, username, password }: CreateUserInput): Promise<CreateUserOutput> {
     try {
+
       // ! 이미 데이터베이스에 닉네임과 이메일이 존재하는지 확인
       const exists = await this.prisma.user.findFirst({
         where: {
@@ -41,6 +50,16 @@ export class UsersService {
           ]
         }
       });
+      if (exists) {
+        // ! 유저 네임이 이미 존재할 경우
+        this.log.logger().error(`${this.log.loggerInfo("유저 네임이 이미 존재할 경우")}`);
+        return {
+          ok: false,
+          error: "existError"
+        };
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 10);
 
       await this.prisma.user.create({
         data: {
@@ -48,13 +67,17 @@ export class UsersService {
           lastName,
           email,
           username,
-          password
+          password: hashedPassword
         }
       });
+
       return {
         ok: true
       };
     } catch (error) {
+      //! extraError
+      const { message, name, stack } = error;
+      this.log.logger().error(`${this.log.loggerInfo("extraError", message, name, stack)}`);
       return {
         ok: false,
         error: "Could not create account"
