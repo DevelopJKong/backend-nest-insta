a876d8fb6d9ad0982ca74123b23e4cc8f6c81840
cafe small house <ybaram40@gmail.com>
2022-12-25 17:14:09 +0900

    class-validator 문제점 찾기

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,6 +3,7 @@ import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
 
 @Module({
-  providers: [UsersService, UsersResolver]
+  providers: [UsersService, UsersResolver],
+  exports:[UsersService]
 })
 export class UsersModule {}
