8dfb0a2f8b9909678c71686fb5ae3011aec5b160
cafe small house <ybaram40@gmail.com>
2022-12-30 21:05:59 +0900

    interceptor 주석

--- src/main.ts
+++ src/main.ts
@@ -6,19 +6,25 @@ import * as fs from 'fs';
 import * as express from 'express';
 import { BACKEND_URL, PORT, fileFolder } from './common/common.constants';
 import { join } from 'path';
+// ! LoggerInterceptor 사용 시
 // import { LoggerInterceptor } from './libs/logger/logger.interceptor';
 // import { LoggerService } from './libs/logger/logger.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   if (process.env.NODE_ENV === 'dev') {
     if (!fs.existsSync(fileFolder)) fs.mkdirSync(fileFolder);
   }
+  
   app.useGlobalPipes(new ValidationPipe());
-  // app.useGlobalInterceptors(new LoggerInterceptor(new LoggerService({ nodeEnv: process.env.NODE_ENV })));
   app.use(graphqlUploadExpress({ maxFileSize: 1000000, maxFiles: 10 }));
   app.use('/files', express.static(join(__dirname, '../files')));
   app.enableCors();
+
+    // ! LoggerInterceptor 사용 시
+  // app.useGlobalInterceptors(new LoggerInterceptor(new LoggerService({ nodeEnv: process.env.NODE_ENV })));
+
   const start = () => console.log(`Server Start! ${BACKEND_URL}`);
   await app.listen(PORT, start);
 }
