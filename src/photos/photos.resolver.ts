5bee9d0469704ca5c06c089c163b92dfdd0128ba
cafe small house <ybaram40@gmail.com>
2023-01-07 12:11:38 +0900

    #6.4 Upload Photo part Three 완료

--- src/photos/photos.resolver.ts
+++ src/photos/photos.resolver.ts
@@ -1,13 +1,16 @@
+import { RoleData } from '@prisma/client';
 import { User } from './../users/entities/user.entity';
 import { Args, Mutation, Resolver } from '@nestjs/graphql';
 import { UploadPhotoOutput, UploadPhotoInput } from './dtos/upload-photo.dto';
 import { AuthUser } from 'src/libs/auth/auth-user.decorator';
 import { PhotosService } from './photos.service';
+import { Role } from 'src/libs/auth/role.decorator';
 
 @Resolver()
 export class PhotosResolver {
   constructor(private readonly photosService: PhotosService) {}
   @Mutation(_return => UploadPhotoOutput)
+  @Role([RoleData.USER])
   async uploadPhoto(
     @AuthUser() authUser: User,
     @Args('input') uploadPhotoInput: UploadPhotoInput,
--- src/photos/photos.service.ts
+++ src/photos/photos.service.ts
@@ -1,17 +1,41 @@
+import { PrismaService } from './../prisma/prisma.service';
 import { UploadPhotoInput, UploadPhotoOutput } from './dtos/upload-photo.dto';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PhotosService {
+  constructor(private readonly prisma: PrismaService) {}
   async uploadPhoto(userId: number, { photoFile, caption }: UploadPhotoInput): Promise<UploadPhotoOutput> {
     try {
+      let hashtagObj = [];
+
       if (caption) {
-        // ! parse caption
-        // ! get or create Hashtag
+        const hashtags = caption.match(/#[\w]+/g);
+        hashtagObj = hashtags.map(hashtag => ({ where: { hashtag }, create: { hashtag } }));
       }
 
+      // ! parse caption
+      // ! get or create Hashtag
+
+      const { filename } = await photoFile;
       // ! save the photo WITH the parsed hashtags
       // ! add the photo the hashtags
+      await this.prisma.photo.create({
+        data: {
+          file: filename,
+          caption,
+          user: {
+            connect: {
+              id: userId,
+            },
+          },
+          ...(hashtagObj.length > 0 && {
+            hashtags: {
+              connectOrCreate: hashtagObj,
+            },
+          }),
+        },
+      });
 
       return {
         ok: true,
